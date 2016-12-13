5f281d29ff6d486ce1f0dc5a43efe0273f0952c3
NanHeSam <samhenan@gmail.com>
2016-12-13 13:56:51 -0500

    added entity framework and made small change to RouteConfig.cs

--- RainCheckV2/RainCheckV2/App_Start/RouteConfig.cs
+++ RainCheckV2/RainCheckV2/App_Start/RouteConfig.cs
@@ -12,7 +12,12 @@ namespace RainCheckV2
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
+            routes.IgnoreRoute("{resource}.config");
+            routes.MapRoute(
+                name: "Home",
+                url: "",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
